bdfe9adbed57324a46e3116272abda61f001731f
Prerna Premkumar <prernaomega@gmail.com>
2024-09-17 09:09:22 +0530

    Update Pdf 4 - Bruteforce Substring Check.py

--- Pdf 4 - Bruteforce Substring Check.py
+++ Pdf 4 - Bruteforce Substring Check.py
@@ -1,14 +1,10 @@
 def stringMatching(words):
     result = []
-    
-    # Iterate through each word
     for i in range(len(words)):
-        # Compare the current word with every other word
         for j in range(len(words)):
-            # Avoid comparing the word with itself and check if it's a substring
             if i != j and words[i] in words[j]:
                 result.append(words[i])
-                break  # No need to check further if we found a match
+                break  
     
     return result
 print(stringMatching(["superhero","super","hello","hell"]))
