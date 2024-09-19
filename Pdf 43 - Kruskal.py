d693e124117b079c62ef1859f2fd3b0bfeb968f3
Prerna Premkumar <prernaomega@gmail.com>
2024-09-19 07:45:37 +0530

    Create Pdf 43 - Kruskal.py

---
+++ Pdf 43 - Kruskal.py
@@ -0,0 +1,47 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+def kruskal(n, edges):
+    edges.sort(key=lambda x: x[2])
+    uf = UnionFind(n)
+    mst = []
+    total_weight = 0
+
+    for u, v, weight in edges:
+        if uf.find(u) != uf.find(v):
+            uf.union(u, v)
+            mst.append((u, v, weight))
+            total_weight += weight
+
+    return mst, total_weight
+
+# Test cases
+edges1 = [(0, 1, 10), (0, 2, 6), (0, 3, 5), (1, 3, 15), (2, 3, 4)]
+mst1, weight1 = kruskal(4, edges1)
+print("Edges in MST:", mst1)
+print("Total weight of MST:", weight1)
+
+edges2 = [(0, 1, 2), (0, 3, 6), (1, 2, 3), (1, 3, 8), (1, 4, 5), (2, 4, 7), (3, 4, 9)]
+mst2, weight2 = kruskal(5, edges2)
+print("Edges in MST:", mst2)
+print("Total weight of MST:", weight2)
+      
